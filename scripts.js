dbb4a4ac37913637ff8c59d20a137eeb7d3a6336
Emanuel Marinho <psic.emanuelmarinho@gmail.com>
2025-03-28 20:57:40 -0300

    Concluindo parcialmente o js e o projeto, ainda coisas a melhorar

--- scripts.js
+++ scripts.js
@@ -1,8 +1,81 @@
 const product = document.getElementById("product")
 const btn = document.getElementById("mainBtn")
+const ul = document.getElementsByTagName("ul")[0]
+const divRemove = document.getElementsByClassName("item-remove")[0]
+const btnWarning = document.querySelector("div img:nth-child(3)")
 
-btn.addEventListener("click", () => {
-    console.log(product.value)
+
+function addItem() {
+    if(product.value.trim() === "") return alert("Digite um produto")
+
+    const li = document.createElement("li")
+    const divChecked = document.createElement("div")
+    divChecked.classList.add("checked")
+    const span = document.createElement("span")
+    span.innerText = product.value
+    const img = document.createElement("img")
+    img.src = "assets/icons/trash.svg"
+    img.alt = "lixeira"
+
+    li.appendChild(divChecked)
+    li.appendChild(span)
+    li.appendChild(img)
+    ul.appendChild(li)
 
     product.value = ""
+    product.focus()
+
+    img.addEventListener("click", ()=> {
+        li.remove()
+        divRemove.classList.remove("none")
+    })
+    
+    contSelectedItems()
+}
+
+ul.addEventListener("click", (event) => {
+    if(event.target.classList.contains("checked")) {
+        event.target.classList.toggle("checked-active")
+        contSelectedItems()
+    }
 })
+
+ul.addEventListener("click", (event)=>{
+    if(event.target.tagName === "IMG") {
+        const selectedItems = document.querySelectorAll(".checked-active")
+
+        if(selectedItems.length > 0) {
+            selectedItems.forEach((item)=> {
+                item.parentElement.remove()
+            })
+        } else {
+            event.target.parentElement.remove()
+        }
+
+        divRemove.classList.remove("none")
+        contSelectedItems()
+    }
+})
+
+btn.addEventListener("click", addItem)
+
+product.addEventListener("keypress", (event) => {
+    if(event.key === "Enter") {
+        addItem()
+    }
+})
+
+removeWarning()
+
+
+function removeWarning() {
+    btnWarning.addEventListener("click", () => {
+        divRemove.classList.add("none")
+    })
+}
+
+function contSelectedItems() {
+    const selectedCount = document.querySelectorAll(".checked-active").length
+    console.log(`Itens selecionados: ${selectedCount}`)
+}
+
